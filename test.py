f650218dbf1589c8f1078ea05efb74c1ed0ee914
Loïc Grobol <loic.grobol@gmail.com>
2018-11-16 15:11:41 +0100

    Use CLA learning rate (so 0.01 default instead of 0.001)

--- test.py
+++ test.py
@@ -123,7 +123,7 @@ def run(train_batch_size, val_batch_size, epochs, lr, momentum, log_interval, de
     train_loader, val_loader = get_data_loaders(train_batch_size, val_batch_size, vectors, device)
     model = Net(out_dim=3, pretrained_embeddings=vectors.vectors).to(device)
 
-    optimizer = torch.optim.Adam(model.parameters())
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     trainer = ignite.engine.create_supervised_trainer(
         model,
         optimizer,
